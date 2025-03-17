ed6e93d0f9c808223efb154700d5dc64506a2674
akshat0012 <akshatchaudhary1222@gmail.com>
2025-03-18 00:14:35 +0530

    enhance: improve dark/light theme toggle (#4)

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,21 +1,18 @@
 "use client";
 
 import { useTheme } from "./theme-provider";
+import { Moon, Sun } from 'lucide-react';
+import { Button } from "@/components/ui/button"
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <button
+    <Button variant="outline" size="sm" 
+      className="flex items-center gap-1"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      style={{
-        "height": "36px",
-        "width": "116px",
-        fontSize: "14px",
-        fontFamily: "Arial, sans-serif"
-      }}
     >
-      {theme === "light" ? "🌙 Dark Mode" : "☀️ Light Mode"}
-    </button>
+      {theme === "light" ? <><Moon size={16}/> Dark Theme</> : <><Sun size={16}/> Light Theme</>}
+    </Button>
   );
 }
